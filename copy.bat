98c811f0fecd371ac1c9545278a9f52625d3a42c
peter-lyr <llydrp.ldp@gmail.com>
2023-05-21 22:10:36 +0800

    copy.bat

---
+++ copy.bat
@@ -0,0 +1,15 @@
+@echo off
+echo [%~0]
+%~d0
+cd %~dp0
+set dp=%~dp0
+set /p nvimwin64=nvim-win64 abspath: 
+if defined nvimwin64 (
+  set pack=%nvimwin64%\share\nvim\runtime\pack\
+)
+if exist %pack% (
+  xcopy %dp% %pack%nvimconfig\ /s /e /f
+) else (
+  echo no "%pack%"
+)
+pause
