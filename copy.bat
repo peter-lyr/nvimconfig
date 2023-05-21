95e19539e4716cd6e79a69b99b7fe7ef1c88638b
peter-lyr <llydrp.ldp@gmail.com>
2023-05-21 23:48:42 +0800

    /Y 禁止提示以确认改写一个现存目标文件

--- copy.bat
+++ copy.bat
@@ -8,7 +8,7 @@ if defined nvimwin64 (
   set pack=%nvimwin64%\share\nvim\runtime\pack\
 )
 if exist %pack% (
-  xcopy %dp% %pack%nvimconfig\ /s /e /f /h
+  xcopy %dp% %pack%nvimconfig\ /s /e /f /h /y
 ) else (
   echo no "%pack%"
 )
