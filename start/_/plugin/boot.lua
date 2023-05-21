46d66994e88bc78b087b3456f10dcb7440fd3e98
peter-lyr <llydrp.ldp@gmail.com>
2023-05-21 23:27:45 +0800

    ui nvim-dev-icon

--- start/_/plugin/boot.lua
+++ start/_/plugin/boot.lua
@@ -54,5 +54,21 @@ lazy.setup({
   },
   checker = {
     enabled = false,
-  }
+  },
+  ui = {
+    icons = {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
 })
