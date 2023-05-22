670a5792a8b5d4a927d2ae0ac6a3093b656d0dfd
peter-lyr <llydrp.ldp@gmail.com>
2023-05-22 19:52:16 +0800

    nvim-tree.lua"

---
+++ start/_/lua/plugins/nvimtree.lua
@@ -0,0 +1,11 @@
+vim.keymap.set({ 'n', 'v' }, '<leader>;', ':<c-u>NvimTreeToggle<cr>', { silent = true })
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  lazy = true,
+  cmd = { 'NvimTreeToggle', },
+  event = { 'FocusLost', },
+  config = function()
+    require('nvim-tree').setup()
+  end
+}
