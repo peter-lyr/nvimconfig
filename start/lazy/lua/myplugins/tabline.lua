b444b41ca685f5d47720a22f8cae517414cd5350
peter-lyr <llydrp.ldp@gmail.com>
2023-05-23 20:11:32 +0800

    tabline

---
+++ start/lazy/lua/myplugins/tabline.lua
@@ -0,0 +1,19 @@
+-- local name = "tabline"
+
+return {
+  -- name = name,
+  -- dir = vim.fn.expand('$VIMRUNTIME') .. '\\pack\\nvimconfig\\opt\\' .. name,
+  -- event = { 'WinEnter', 'VimResized', 'FocusLost' },
+  -- dependencies = {
+  --   "dbakker/vim-projectroot",
+  --   config = function()
+  --     vim.g.rootmarkers = {
+  --       ".cache",
+  --       "build",
+  --       "compile_commands.json",
+  --       "CMakeLists.txt",
+  --       ".git",
+  --     }
+  --   end
+  -- },
+}
