d8d7bc5bf21a6d9268b112bbeed28816952274a2
peter-lyr <llydrp.ldp@gmail.com>
2023-05-23 14:04:00 +0800

    leader leader l

--- start/lazy/lua/plugins/nvimtree.lua
+++ start/lazy/lua/plugins/nvimtree.lua
@@ -1,10 +1,11 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>;', ':<c-u>NvimTreeToggle<cr>', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>l', ':<c-u>NvimTreeFindFileToggle<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader><leader>l', ':<c-u>NvimTreeOpen <c-r>=getcwd()<cr><cr>', { silent = true })
 
 return {
   'nvim-tree/nvim-tree.lua',
   lazy = true,
-  cmd = { 'NvimTreeToggle', },
+  cmd = { 'NvimTreeToggle', 'NvimTreeFindFileToggle', 'NvimTreeOpen', },
   event = { 'FocusLost', },
   config = function()
     local sta, config = pcall(require, 'nvimtree')
