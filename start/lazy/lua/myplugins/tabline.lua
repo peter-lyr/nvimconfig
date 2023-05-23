cbe74df2a1f3957269d0706b0ca2a84457783051
peter-lyr <llydrp.ldp@gmail.com>
2023-05-24 00:29:32 +0800

    tabline ahmedkhalf/project.nvim

--- start/lazy/lua/myplugins/tabline.lua
+++ start/lazy/lua/myplugins/tabline.lua
@@ -1,5 +1,13 @@
 local name = "tabline"
 
+local patterns = {
+  ".cache",
+  "build",
+  "compile_commands.json",
+  "CMakeLists.txt",
+  ".git",
+}
+
 vim.g.startuptime = os.time()
 
 vim.keymap.set({ 'n', 'v' }, '<leader>bU', ':<c-u>TablineSessionRestoreProjects<cr>', { silent = true })
@@ -11,15 +19,22 @@ return {
   event = { 'WinEnter', 'VimResized', 'FocusLost' },
   cmd = { 'TablineSessionRestoreProjects', 'TablineSessionRestoreAll', },
   dependencies = {
-    "dbakker/vim-projectroot",
-    config = function()
-      vim.g.rootmarkers = {
-        ".cache",
-        "build",
-        "compile_commands.json",
-        "CMakeLists.txt",
-        ".git",
-      }
-    end
+    {
+      "dbakker/vim-projectroot",
+      config = function()
+        vim.g.rootmarkers = patterns
+      end
+    },
+    {
+      'ahmedkhalf/project.nvim',
+      config = function()
+        require("project_nvim").setup({
+          manual_mode = false,
+          datapath = vim.fn['expand']("$VIMRUNTIME") .. "\\my-neovim-data",
+          detection_methods = { "pattern", "lsp" },
+          patterns = patterns,
+        })
+      end
+    },
   },
 }
