a143226eb24928c9a5e62a6d33ca055d47df1626
peter-lyr <llydrp.ldp@gmail.com>
2023-05-24 14:51:22 +0800

    local

--- opt/tabswitch/plugin/tabswitch.lua
+++ opt/tabswitch/plugin/tabswitch.lua
@@ -1,41 +1,35 @@
-local a = vim.api
-local c = vim.cmd
-local s = vim.keymap.set
-
 local lasttab = 0
 
-local opt = { silent = true }
-
-s({ 'n', 'v', }, '<leader><f1>', '1gt', opt)
-s({ 'n', 'v', }, '<leader><f2>', '2gt', opt)
-s({ 'n', 'v', }, '<leader><f3>', '3gt', opt)
-s({ 'n', 'v', }, '<leader><f4>', '4gt', opt)
-s({ 'n', 'v', }, '<leader><f5>', '5gt', opt)
-s({ 'n', 'v', }, '<leader><f6>', '6gt', opt)
-s({ 'n', 'v', }, '<leader><f7>', '7gt', opt)
-s({ 'n', 'v', }, '<leader><f8>', '8gt', opt)
-s({ 'n', 'v', }, '<leader><f9>', '9gt', opt)
-s({ 'n', 'v', }, '<leader><f10>', '10gt', opt)
-s({ 'n', 'v', }, '<leader><f11>', '11gt', opt)
-s({ 'n', 'v', }, '<leader><f12>', '<cmd>:tablast<cr>', opt)
+vim.keymap.set({ 'n', 'v', }, '<leader><f1>', '1gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f2>', '2gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f3>', '3gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f4>', '4gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f5>', '5gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f6>', '6gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f7>', '7gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f8>', '8gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f9>', '9gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f10>', '10gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f11>', '11gt', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<leader><f12>', '<cmd>:tablast<cr>', { silent = true })
 
 local space_enter = function()
   if lasttab ~= 0 then
-    pcall(c, "tabn " .. lasttab)
+    pcall(vim.cmd, "tabn " .. lasttab)
   end
 end
 
-s({ 'n', 'v', }, '<leader><cr>', space_enter, opt)
+vim.keymap.set({ 'n', 'v', }, '<leader><cr>', space_enter, { silent = true })
 
-s({ 'n', 'v' }, '<cr>', ':<c-u>tabnext<cr>', opt)
-s({ 'n', 'v' }, '<s-cr>', ':<c-u>tabprevious<cr>', opt)
+vim.keymap.set({ 'n', 'v' }, '<cr>', ':<c-u>tabnext<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<s-cr>', ':<c-u>tabprevious<cr>', { silent = true })
 
-s({ 'n', 'v', }, '<c-s-h>', ':<c-u>try <bar> tabmove - <bar> catch <bar> endtry<cr>', opt)
-s({ 'n', 'v', }, '<c-s-l>', ':<c-u>try <bar> tabmove + <bar> catch <bar> endtry<cr>', opt)
-s({ 'n', 'v', }, '<c-s-k>', 'gT', opt)
-s({ 'n', 'v', }, '<c-s-j>', 'gt', opt)
+vim.keymap.set({ 'n', 'v', }, '<c-s-h>', ':<c-u>try <bar> tabmove - <bar> catch <bar> endtry<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<c-s-l>', ':<c-u>try <bar> tabmove + <bar> catch <bar> endtry<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<c-s-k>', 'gT', { silent = true })
+vim.keymap.set({ 'n', 'v', }, '<c-s-j>', 'gt', { silent = true })
 
-a.nvim_create_autocmd({ "TabLeave" }, {
+vim.api.nvim_create_autocmd({ "TabLeave" }, {
   callback = function()
     lasttab = vim.fn['tabpagenr']()
   end
