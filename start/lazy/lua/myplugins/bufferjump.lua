ea6ed2ea9b6a443759045192063d7687f37f4bf2
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 00:33:31 +0800

    .

--- start/lazy/lua/myplugins/bufferjump.lua
+++ start/lazy/lua/myplugins/bufferjump.lua
@@ -6,10 +6,24 @@ return {
   event = { 'FocusLost' },
   keys = {
     '<leader>p',
-    '<leader>w', '<leader>s', '<leader>a', '<leader>d',
-    '<leader>o', '<leader>i', '<leader>u',
-    '<leader><leader>o', '<leader><leader>i',
+
+    '<leader>w',
+    '<leader>s',
+    '<leader>a',
+    '<leader>d',
+
+    '<leader>o',
+    '<leader>i',
+    '<leader>u',
+
+    '<leader><leader>o',
+    '<leader><leader>i',
+
     '<leader><leader><leader>i',
-    '<leader><leader>w', '<leader><leader>s', '<leader><leader>d', '<leader><leader>a',
+
+    '<leader><leader>w',
+    '<leader><leader>s',
+    '<leader><leader>d',
+    '<leader><leader>a',
   }
 }
