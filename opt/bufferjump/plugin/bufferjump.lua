8759ad45447145a8c00f8946d42d8bc2810f8f22
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 00:31:16 +0800

    .

--- opt/bufferjump/plugin/bufferjump.lua
+++ opt/bufferjump/plugin/bufferjump.lua
@@ -1,20 +1,22 @@
-vim.keymap.set({ 'n', 'v' }, '<leader>p', '<c-w>p', { silent = true })
+-- '<,'>s/vim\.keymap\.set(\([^}]\+},\) *\([^,]\+,\) *\([^,]\+,\) *\([^)]\+\))/\=printf("vim.keymap.set(%-20s %-28s %-64s %s)", submatch(1), submatch(2), submatch(3), submatch(4))
 
-vim.keymap.set({ 'n', 'v' }, '<leader>w', ':<c-u>call bufferjump#up(1)<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>s', ':<c-u>call bufferjump#down(1)<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>a', ':<c-u>call bufferjump#left(1)<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>d', ':<c-u>call bufferjump#right(1)<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>p',                 '<c-w>p',                                                        { silent = true })
 
-vim.keymap.set({ 'n', 'v' }, '<leader>o', ':<c-u>call bufferjump#maxheight()<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>i', ':<c-u>call bufferjump#samewidthheight()<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader>u', ':<c-u>call bufferjump#maxwidth()<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>w',                 ':<c-u>call bufferjump#up(1)<cr>',                               { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>s',                 ':<c-u>call bufferjump#down(1)<cr>',                             { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>a',                 ':<c-u>call bufferjump#left(1)<cr>',                             { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>d',                 ':<c-u>call bufferjump#right(1)<cr>',                            { silent = true })
 
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>o', ':<c-u>call bufferjump#miximize(1)<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>i', ':<c-u>call bufferjump#miximize(0)<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>o',                 ':<c-u>call bufferjump#maxheight()<cr>',                         { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>i',                 ':<c-u>call bufferjump#samewidthheight()<cr>',                   { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader>u',                 ':<c-u>call bufferjump#maxwidth()<cr>',                          { silent = true })
 
-vim.keymap.set({ 'n', 'v' }, '<leader><leader><leader>i', ':<c-u>call bufferjump#samewidthheightfix()<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>o',         ':<c-u>call bufferjump#miximize(1)<cr>',                         { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>i',         ':<c-u>call bufferjump#miximize(0)<cr>',                         { silent = true })
 
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>w', ':<c-u>call bufferjump#winfixheight()<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>s', ':<c-u>call bufferjump#nowinfixheight()<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>d', ':<c-u>call bufferjump#winfixwidth()<cr>', { silent = true })
-vim.keymap.set({ 'n', 'v' }, '<leader><leader>a', ':<c-u>call bufferjump#nowinfixwidth()<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader><leader>i', ':<c-u>call bufferjump#samewidthheightfix()<cr>',                { silent = true })
+
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>w',         ':<c-u>call bufferjump#winfixheight()<cr>',                      { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>s',         ':<c-u>call bufferjump#nowinfixheight()<cr>',                    { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>d',         ':<c-u>call bufferjump#winfixwidth()<cr>',                       { silent = true })
+vim.keymap.set({ 'n', 'v' },        '<leader><leader>a',         ':<c-u>call bufferjump#nowinfixwidth()<cr>',                     { silent = true })
