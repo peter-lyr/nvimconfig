641680405907b839f66ef3f0db64558d4585be6c
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 00:25:15 +0800

    .

--- start/lazy/lua/myplugins/statusline.lua
+++ start/lazy/lua/myplugins/statusline.lua
@@ -3,7 +3,7 @@ local name = "statusline"
 return {
   name = name,
   dir = vim.fn.expand('$VIMRUNTIME') .. '\\pack\\nvimconfig\\opt\\' .. name,
-  event = { 'WinEnter', 'VimResized', 'FocusLost' },
+  event = { 'FocusLost', 'WinEnter', 'VimResized', },
   dependencies = {
     "itchyny/vim-gitbranch",
   },
