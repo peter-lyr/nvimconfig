f0adf606e1d872233bb691ecc80da8a575a01afe
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 00:30:05 +0800

    .

--- start/lazy/lua/plugins/nvimtree.lua
+++ start/lazy/lua/plugins/nvimtree.lua
@@ -2,7 +2,11 @@ return {
   'nvim-tree/nvim-tree.lua',
   lazy = true,
   event = { 'FocusLost', },
-  keys = { '<leader>;', '<leader>l', '<leader><leader>l', },
+  keys = {
+    '<leader>;',
+    '<leader>l',
+    '<leader><leader>l',
+  },
   config = function()
     require('config.nvimtree')
   end,
