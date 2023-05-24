5bdcf54cd5659ab41dbdbd83dad9a8630e2864f1
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 00:23:56 +0800

    tabswitch keys

--- start/lazy/lua/myplugins/tabswitch.lua
+++ start/lazy/lua/myplugins/tabswitch.lua
@@ -4,4 +4,26 @@ return {
   name = name,
   dir = vim.fn.expand('$VIMRUNTIME') .. '\\pack\\nvimconfig\\opt\\' .. name,
   event = { 'TabNew', 'FocusLost' },
+  keys = {
+    '<leader><f1>',
+    '<leader><f2>',
+    '<leader><f3>',
+    '<leader><f4>',
+    '<leader><f5>',
+    '<leader><f6>',
+    '<leader><f7>',
+    '<leader><f8>',
+    '<leader><f9>',
+    '<leader><f10>',
+    '<leader><f11>',
+    '<leader><f12>',
+
+    '<leader><cr>',
+
+    '<cr>',
+    '<s-cr>',
+
+    '<c-s-h>',
+    '<c-s-l>',
+  }
 }
