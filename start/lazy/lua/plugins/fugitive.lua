89c63da7e7b023422dc4cc29fd1d80d3d0bda246
peter-lyr <llydrp.ldp@gmail.com>
2023-05-26 00:13:39 +0800

    fugitive

---
+++ start/lazy/lua/plugins/fugitive.lua
@@ -0,0 +1,15 @@
+return {
+  'tpope/vim-fugitive',
+  lazy = true,
+  event = { 'FocusLost', },
+  keys = {
+    '<leader>gg',
+    '<leader>gA',
+    '<leader>ga',
+  },
+  config = function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>gg', ':<c-u>Git<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>gA', ':<c-u>Git add -A<cr>', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>ga', ':<c-u>Git add %<cr>', { silent = true })
+  end,
+}
