bad2521ddb7ccbd614a73fc994f9906173fd472b
peter-lyr <llydrp.ldp@gmail.com>
2023-05-25 14:23:42 +0800

    events bufenter

---
+++ opt/core/plugin/events.lua
@@ -0,0 +1,22 @@
+local lang = {
+  'c',
+  'cpp',
+  'python',
+  'ld',
+}
+
+local tab_width = function()
+  if vim.tbl_contains(lang, vim.opt.filetype:get()) == true then
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.shiftwidth = 4
+  else
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = tab_width,
+})
