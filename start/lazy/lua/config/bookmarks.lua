1cff928465bda4f3932e33ce2d7347a29328d37e
peter-lyr <llydrp.ldp@gmail.com>
2023-05-31 14:58:42 +0800

    saveBookmarks on change

--- start/lazy/lua/config/bookmarks.lua
+++ start/lazy/lua/config/bookmarks.lua
@@ -6,6 +6,11 @@ local rep = function(path)
   return path
 end
 
+local fn = function(f)
+  f()
+  actions.saveBookmarks()
+end
+
 local setup = function()
   local curcwd = vim.fn.tolower(rep(vim.fn['projectroot#get'](vim.api.nvim_buf_get_name(0))))
   local path = (#curcwd > 0 and curcwd or vim.fn.expand("$HOME")) .. "/.bookmarks"
@@ -29,12 +34,12 @@ local setup = function()
         return
       end
       actions.refresh()
-      vim.keymap.set("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
-      vim.keymap.set("n", "mi", bm.bookmark_ann)    -- add or edit mark annotation at current line
-      vim.keymap.set("n", "mc", bm.bookmark_clean)  -- clean all marks in local buffer
-      vim.keymap.set("n", "ms", bm.bookmark_next)   -- jump to next mark in local buffer
-      vim.keymap.set("n", "mw", bm.bookmark_prev)   -- jump to previous mark in local buffer
-      vim.keymap.set("n", "ml", bm.bookmark_list)   -- show marked file list in quickfix window
+      vim.keymap.set("n", "mm", function() fn(bm.bookmark_toggle) end) -- add or remove bookmark at current line
+      vim.keymap.set("n", "mi", function() fn(bm.bookmark_ann) end)    -- add or edit mark annotation at current line
+      vim.keymap.set("n", "mc", function() fn(bm.bookmark_clean) end)  -- clean all marks in local buffer
+      vim.keymap.set("n", "ms", bm.bookmark_next)                      -- jump to next mark in local buffer
+      vim.keymap.set("n", "mw", bm.bookmark_prev)                      -- jump to previous mark in local buffer
+      vim.keymap.set("n", "ml", bm.bookmark_list)                      -- show marked file list in quickfix window
     end
   })
   vim.fn.timer_start(200, function() actions.refresh() end)
