a82f6209546344cce09e8c41b76da0f376814486
peter-lyr <llydrp.ldp@gmail.com>
2023-06-01 12:47:48 +0800

    .

--- start/lazy/lua/myplugins/multilinesearch.lua
+++ start/lazy/lua/myplugins/multilinesearch.lua
@@ -3,7 +3,5 @@ local name = "multilinesearch"
 return {
   name = name,
   dir = vim.fn.expand('$VIMRUNTIME') .. '\\pack\\nvimconfig\\opt\\' .. name,
-  keys = {
-    '<c-8>',
-  },
+  event = { 'FocusLost', 'CursorMoved', },
 }
