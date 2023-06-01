07ce4fa9a5a2b628a948e47e634a3c9fe6669f84
peter-lyr <llydrp.ldp@gmail.com>
2023-06-01 19:48:00 +0800

    statuscol.lua

---
+++ start/lazy/lua/plugins/statuscol.lua
@@ -0,0 +1,24 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  config = function()
+    vim.opt.foldcolumn = 1
+    local builtin = require("statuscol.builtin")
+    require("statuscol").setup({
+      bt_ignore = { "terminal", "nofile" },
+      relculright = true,
+      segments = {
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        {
+          sign = { name = { "Diagnostic" }, maxwidth = 2, colwidth = 1, auto = true },
+          click = "v:lua.ScSa"
+        },
+        {
+          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, wrap = true, auto = true },
+          click = "v:lua.ScSa"
+        },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        -- {text = {"│"}}
+      },
+    })
+  end,
+}
