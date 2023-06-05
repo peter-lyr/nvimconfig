aa3dda45308eb7cc79bc9096150c57c4220611bf
peter-lyr <llydrp.ldp@gmail.com>
2023-06-05 09:43:42 +0800

    notify background_colour

--- start/lazy/lua/plugins/noice.lua
+++ start/lazy/lua/plugins/noice.lua
@@ -25,6 +25,14 @@ return {
   },
   dependencies = {
     "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
+    {
+
+      "rcarriga/nvim-notify",
+      config = function ()
+        require("notify").setup({
+          background_colour = "#000000",
+        })
+      end
+    }
   },
 }
