f21bf3aab43efb39e250f19210400ac0853bd96e
peter-lyr <llydrp.ldp@gmail.com>
2023-06-05 19:47:49 +0800

    <f3><f3>

--- start/lazy/lua/config/markdownpreview.lua
+++ start/lazy/lua/config/markdownpreview.lua
@@ -5,4 +5,11 @@ vim.g.mkdp_markdown_css = pp:joinpath('mkdp_markdown.css').filename
 vim.g.mkdp_theme = 'light'
 vim.g.mkdp_auto_close = 0
 
-vim.keymap.set({ 'n', 'v' }, '<f3><f3>', ":<c-u>MarkdownPreviewToggle<cr>", { silent = true })
+local _ = function()
+  vim.cmd('h h')
+  vim.cmd('bw')
+  vim.cmd('MarkdownPreviewToggle')
+  vim.keymap.set({ 'n', 'v' }, '<f3><f3>', ":<c-u>MarkdownPreviewToggle<cr>", { silent = true })
+end
+
+vim.keymap.set({ 'n', 'v' }, '<f3><f3>', _, { silent = true })
