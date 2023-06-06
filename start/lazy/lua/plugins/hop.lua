57effdf84a234e07b7cca4b89419ebd250a8abf8
peter-lyr <llydrp.ldp@gmail.com>
2023-06-06 22:58:47 +0800

    hop no f

--- start/lazy/lua/plugins/hop.lua
+++ start/lazy/lua/plugins/hop.lua
@@ -1,14 +1,11 @@
 return {
   'phaazon/hop.nvim',
   lazy = true,
-  event = { 'FocusLost', },
   keys = {
     's',
-    'f',
   },
   config = function()
     require('hop').setup()
-    vim.keymap.set('n', 's', ':HopChar1<cr>',  { silent = true })
-    vim.keymap.set('n', 'f', ':HopChar2<cr>',  { silent = true })
+    vim.keymap.set('n', 's', ':HopChar1<cr>', { silent = true })
   end,
 }
