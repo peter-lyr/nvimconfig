1a6af53c45efc078b53ff88020aee2bd8272491e
peter-lyr <llydrp.ldp@gmail.com>
2023-06-06 18:57:22 +0800

    markdownpreview.css

--- start/lazy/lua/config/markdownpreview.lua
+++ start/lazy/lua/config/markdownpreview.lua
@@ -1,7 +1,7 @@
 local p = require("plenary.path")
 local pp = p:new(vim.g.boot_lua):parent():parent():joinpath('lua', 'config')
 
-vim.g.mkdp_markdown_css = pp:joinpath('mkdp_markdown.css').filename
+vim.g.mkdp_markdown_css = pp:joinpath('markdownpreview.css').filename
 vim.g.mkdp_theme = 'light'
 vim.g.mkdp_auto_close = 0
 
