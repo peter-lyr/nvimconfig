29909b6b52a10a4645606e63620114801d98cfc1
peter-lyr <llydrp.ldp@gmail.com>
2023-06-11 13:12:45 +0800

    commenter

--- start/lazy/lua/config/commenter.lua
+++ start/lazy/lua/config/commenter.lua
@@ -3,7 +3,17 @@ vim.g.NERDDefaultAlign = "left"
 vim.g.NERDCommentEmptyLines = 1
 vim.g.NERDTrimTrailingWhitespace = 1
 vim.g.NERDToggleCheckAllLines = 1
-vim.g.NERDAltDelims_c = 1
+
+vim.g.NERDCustomDelimiters = {
+  markdown = {
+    left = '<!--', right = '-->',
+    leftAlt = '[', rightAlt = ']: #',
+  },
+  c = {
+    left = '//', right = '',
+    leftAlt = '/*', rightAlt = '*/',
+  },
+}
 
 vim.keymap.set({ 'n', 'v' }, '<leader>cp', "vip:call nerdcommenter#Comment('x', 'toggle')<CR>",  { silent = true })
 vim.keymap.set({ 'n', 'v' }, '<leader>c}', "V}k:call nerdcommenter#Comment('x', 'toggle')<CR>",  { silent = true })
